e0cbace7fb21065d99423ce875c96364ef15033c
DenerBarros <denerbarros.eng@gmail.com>
2022-09-14 17:26:50 -0300

    Criado o modelo DTO para o repositories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,27 +1,17 @@
 import { Router } from 'express';
-import { v4 as uuidV4 } from 'uuid';
-import { Category } from '../model/Category';
+import { CategoriesRepository } from '../repositories/CategoriesRepostory';
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request,response) => {
     const { name, description } = request.body;
 
-    const category = new Category();
-
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date(),
-    })
+    categoriesRepository.create({name, description});
     
-   
 
-    categories.push(category);
-
-    return response.status(201).json({ category });
+    return response.status(201).send();
 })
 
 export { categoriesRoutes };
