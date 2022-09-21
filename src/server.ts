3214c8c08e671f68996c51b2dae4e00faaee8af7
DenerBarros <denerbarros.eng@gmail.com>
2022-09-21 11:00:44 -0300

    Criado a documentação com swagger - inserido a documentação da rota de category

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 import { router } from './routes';
 
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "../src/swagger.json";
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
